be1300f67fdd71270c02aa6a893bf00415f527e1
hakatashi <hakatasiloving@gmail.com>
2014-07-19 08:32:04 +0900

    Add script

--- helper.js
+++ helper.js
@@ -1,3 +1,17 @@
-var body = document.getElementById('textfield4');
+var smt = document.getElementById('smt');
 
-body.style['background-color'] = 'red';
+// remove existing submit link
+Array.prototype.forEach.call(smt.children, function(element) {
+	smt.removeChild(element);
+});
+
+// make new submit link
+var submit = document.createElement('input');
+submit.setAttribute('type', 'image');
+submit.setAttribute('src', '/images/common/f_8.gif');
+submit.setAttribute('alt', 'ログイン');
+submit.setAttribute('width', 137);
+submit.setAttribute('height', 25);
+
+// append new submit link
+smt.appendChild(submit);
